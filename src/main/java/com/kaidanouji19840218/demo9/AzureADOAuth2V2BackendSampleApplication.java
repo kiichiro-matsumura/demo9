f859529e0bdb8a17a8b3e77d60841bdfc356874f
kiichiro-matsumura <kaidanouji1984@gmail.com>
2021-05-17 16:07:39 +0900

    Initial commit

---
+++ src/main/java/com/kaidanouji19840218/demo9/AzureADOAuth2V2BackendSampleApplication.java
@@ -0,0 +1,12 @@
+package com.kaidanouji19840218.demo9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureADOAuth2V2BackendSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureADOAuth2V2BackendSampleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kaidanouji19840218/demo9/Demo9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaidanouji19840218.demo9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
